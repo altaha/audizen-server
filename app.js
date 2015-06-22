0223b45273ff06e556cf78e44f3f082b98fc69a5
Ahmed AlTaha <ahmedsalam.2008@gmail.com>
2015-06-23 04:37:58 +0800

    Serve index html file with file upload form

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(logger('dev'))
+app.engine('html', require('ejs').renderFile);
 
 // API Endpoints
 app.use('/', apis)
 
 // Web URLs
 app.get('/', function(req, res, next) {
-    res.send('Hello Audizen')
+    res.render('index.html')
 })
+
+// Serve static files
+app.use('/static', express.static('static'));
 
 app.listen(port, function(){
     console.log('Express server listening on port ' + port)
